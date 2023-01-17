e56cf8d63a866fd51eff2319a5ba83dd3c6c0717
HassanTariqAlvi <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-01-17 17:28:22 +0500

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = ' http://node-env.eba-mbkdjpmd.us-east-1.elasticbeanstalk.com';
+const baseUrl = 'http://nodeapi-env.eba-nmpx3itx.us-east-1.elasticbeanstalk.com';
 
 export const showSTD = () => axios.get(`${baseUrl}/`);
 export const createSTD = (std) => axios.post(`${baseUrl}/api/create`, std);
