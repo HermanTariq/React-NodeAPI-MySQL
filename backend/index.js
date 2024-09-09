9ecb95d0f62bd129f1577f8f5d7064fdc3340796
MAmmarRaza <20ntucs1120@student.ntu.edu.pk>
2024-09-10 02:26:02 +0500

    asdf

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const app = express();
 const PORT = 9001;
@@ -9,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 const onConnect = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '12Qq1212',
-    database: 'devops-db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 onConnect.connect((error) => {
